15b8cc5b92d8ae091e6364d17fbb9431d18a2916
soarersiuhc <soarersiuhc@gmail.com>
2016-05-30 16:30:05 -0700

    Delete pikamodule.js

--- freecodecamp/pomodoro/script/pikamodule.js
+++
@@ -1,8 +0,0 @@
-var Pika = function(){
-  this.require = function(name){
-    var code = new Function("exports", readFile(name));
-    var exports = {};
-    code(exports);
-    return exports;
-  }
-}
